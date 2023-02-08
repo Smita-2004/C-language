542124b9e2884ef0632d081fa8dfb39fcded3378
Smita <114800220+12217951@users.noreply.github.com>
2023-02-08 22:47:59 +0530

    Add files via upload

---
+++ area_of_rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+
+    scanf("%d", &a);
+    printf("a is :%d \n ", a);
+    scanf("%d", &b);
+    printf("b is :%d \n", b);
+    float area=a*b;
+    printf("area is :%f", area);
+    return 0;
+}
