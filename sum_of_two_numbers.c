875f2e11e10427d3b820f365a9c70b054bd19897
Smita <114800220+12217951@users.noreply.github.com>
2023-02-11 11:02:33 +0530

    Add files via upload

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+
+int a;
+printf("Enter a  ");
+scanf("%d",&a);
+printf("value of a is : %d \n",a);
+
+int b;
+printf("Enter b  ");
+scanf("%d",&b);
+printf("value of b is : %d \n",b);
+
+int sum=a+b;
+printf("sum of a and b is : %d\n",sum);
+ 
+
+	return 0;
+}
