33ccf08cef54fe1ca9bf57acdca5efe8bbdb9622
Smita <114800220+12217951@users.noreply.github.com>
2023-02-16 09:34:11 +0530

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1;
+    printf("Enter a number: ");
+    scanf("%d",&num1);
+    if(num1==0)
+    {
+        printf("Welcome\n");
+    }
+    else{
+        printf("How are you?");
+    }
+    return 0;
+}
