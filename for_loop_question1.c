d810634e5f3057f8a31bf70a68af85d43c805df4
Smita <114800220+12217951@users.noreply.github.com>
2023-03-02 00:24:41 +0530

    Add files via upload

---
+++ for_loop_question1.c
@@ -0,0 +1,11 @@
+// print the first five numbers starting from zero together with their square.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=5;i++)
+    {
+        printf("Number: %d   It's square: %d \n",i,i*i);
+    }
+    return 0;
+}
