4ca82d01a5ffe418af68cd338f531717297dabf9
Smita <114800220+12217951@users.noreply.github.com>
2023-03-02 00:25:13 +0530

    Add files via upload

---
+++ for_loop_question2.c
@@ -0,0 +1,11 @@
+// display numbers from 1 to 15 using for loop, use i++
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<16;i++) //i++ or i=i+1 cab be used.
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
