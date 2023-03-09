a417c6458cc11db35361c4e35435f5216e2ce5fd
Smita <114800220+12217951@users.noreply.github.com>
2023-03-09 21:40:33 +0530

    Add files via upload

---
+++ formatted_function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=5643;
+    printf("%d\n",a);
+    printf("*%d\n",a);
+    printf("#%d#\n",a);
+    printf("*%-6d*\n",a);
+    printf("*%6d*\n",a);
+    printf("*%06d*\n",a);
+     printf("*%16d*\n",a);
+    return 0;
+}
