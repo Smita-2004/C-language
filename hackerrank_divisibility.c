be787d06fa8760b8eed0c2c982a628e7ab27def8
Smita <114800220+12217951@users.noreply.github.com>
2023-08-20 12:05:01 +0530

    Add files via upload

---
+++ hackerrank_divisibility.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N,A;
+    scanf("%d",&N);
+    printf("%d",N);
+    scanf("%d",&A);
+    printf("%d",A);
+    return 0;
+}
