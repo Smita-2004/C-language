73e5712262ed9cf549cecef98c8bc99360d9696f
Smita <114800220+12217951@users.noreply.github.com>
2023-09-04 15:59:46 +0530

    Add files via upload

---
+++ ternary_operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    (age>=4 )? printf("You can play the game"): printf("You cannot play the game");
+    return 0;
+}
