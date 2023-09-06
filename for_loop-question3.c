2fe63e843b74d7c9f302e71f3fb47fea013f4263
Smita <114800220+12217951@users.noreply.github.com>
2023-09-06 11:24:33 +0530

    Add files via upload

---
+++ for_loop-question3.c
@@ -0,0 +1,14 @@
+// WAP to display even numbers from 0 to 14
+#include<stdio.h>
+int main()
+{
+   int a,i=0;
+   for(i;i<15;i++)
+   {
+    printf("%d\n",i);
+    i=i+1;
+   
+   }
+
+    return 0;
+}
